b988020bc65fd3fd819dfb6a061d49f9f241c194
isaac karanja <isaackimura@gmail.com>
2020-04-14 22:07:28 +0300

    importing firebase file

--- src/routes/app.js
+++ src/routes/app.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 const admin = require('firebase-admin');
-const serviceAccount = require('src/serviceAccountKey.json');
+const serviceAccount = require('src/corona-app-firebaes.json');
 const bodyParser = require('body-parser');
 const path = require('path');
 const fs = require('fs');
